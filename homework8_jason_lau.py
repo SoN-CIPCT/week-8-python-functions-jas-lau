c6a62aa5a658732b9106d33cb1a93cc3ce037f1b
jas-lau <31515199+jas-lau@users.noreply.github.com>
2025-02-23 13:14:21 -0800

    Create homework8_jason_lau.py

---
+++ homework8_jason_lau.py
@@ -0,0 +1,7 @@
+def make_sandwhich (*ingredients):
+    print("Summary of sandwhich ordered:")
+    for ingredient in ingredients:
+        print(ingredient)
+
+make_sandwhich("sourdough", "mayo", "cheese", "lettuce", "turkey")
+make_sandwhich("focaccia", "pesto", "cheese", "arugala", "mushroom")
